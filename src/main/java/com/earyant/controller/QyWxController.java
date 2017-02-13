db221650652085e19bc9dc6a958052615eb30ec4
Earyant <lirui940403@outlook.com>
2017-02-13 17:42:21 +0800

    create

---
+++ src/main/java/com/earyant/controller/QyWxController.java
@@ -0,0 +1,10 @@
+package com.earyant.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lirui on 2017/2/13.
+ */
+@RestController
+public class QyWxController {
+}
